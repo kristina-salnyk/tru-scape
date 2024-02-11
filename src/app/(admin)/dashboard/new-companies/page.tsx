74f242e40fdd368679ebb7efc957c2973a88dd30
Kristina Salnyk <kristina.salnyk@gmail.com>
2024-02-11 12:47:51 +0100

    feature(dashboard): added dashboard page

---
+++ src/app/(admin)/dashboard/new-companies/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '@/app/components/header';
+
+function Page() {
+  return (
+    <>
+      <Header>Dashboard / New Companies</Header>
+      <div>Some additional information</div>
+    </>
+  );
+}
+
+export default Page;
