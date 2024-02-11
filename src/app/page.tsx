96d472f1d431c6ca9c5ea24bbac331b26dbe12cd
Kristina Salnyk <kristina.salnyk@gmail.com>
2024-02-11 12:45:34 +0100

    feature(global-error): added global error handler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import { headers } from 'next/headers';
 import AddCompanyButton from '@/app/components/add-company-button';
+import MagicButton from '@/app/components/magic-button';
 
 export default function Home() {
-  console.log(headers());
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
       <AddCompanyButton />
+      <MagicButton />
     </main>
   );
 }
